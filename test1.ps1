e716207de37dbd064b4f2d7d3b6c6d216608c416
testmailnaga77 <testmailnaga77@gmail.com>
2026-04-27 06:10:43 +0530

    Add files via upload

---
+++ test1.ps1
@@ -0,0 +1,15 @@
+﻿clear-host 
+
+
+for($i = 1; $i -le 5 ; $i++)
+{
+
+    write-host "Good Mornign" -f red 
+    write-host "have a nice day"
+        
+    write-host "Current value of i is $i" -f Yellow
+    write-host "========================="
+
+}
+
+write-host "End of the program"
